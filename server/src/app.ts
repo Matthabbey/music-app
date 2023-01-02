3fb434f561c412170183d64e6591ad23501b4dad
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-01-02 14:11:49 +0100

    server route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,24 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+
 
 const app = express()
 
+// app.use(cors({origin: true}))
+
 app.get("/", (req, res) =>{
     return res.json("hey there now")
+})
+
+mongoose.connect(process.env.MONGO_DB!, {useNewUrlParser: true});
+mongoose.connection
+.once("open", ()=> console.log("Connected"))
+.on("error", (error)=>{
+    console.log(`ERROR: ${error}`);
+    
 })
 
 app.listen(4000, ()=> console.log("listening to port 4000"))
