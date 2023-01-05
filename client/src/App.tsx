48622e2de7199e532c958ac898ce7d81c822ebe6
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-01-05 16:37:25 +0100

    header component for frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,42 @@
-import { getAuth } from 'firebase/auth'
-import React, { useEffect, useState } from 'react'
-import {Route, Routes, useNavigate} from 'react-router-dom'
-import { Home, Login } from './components'
-import { app } from './config/firebase.config'
+import { getAuth } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { Home, Login } from "./components";
+import { app } from "./config/firebase.config";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
-  const firebaseAuth = getAuth(app)
-  const navigate = useNavigate()
-  const [authState, setAuthState] = useState(false)
-  const [auth, setAuth] = useState(false || window.localStorage.getItem('auth') === "true")
+  const firebaseAuth = getAuth(app);
+  const navigate = useNavigate();
+  const [authState, setAuthState] = useState(false);
+  const [auth, setAuth] = useState(
+    false || window.localStorage.getItem("auth") === "true"
+  );
   useEffect(() => {
-    firebaseAuth.onAuthStateChanged((userCred) =>{
-      console.log(userCred)
-      if(userCred){
+    firebaseAuth.onAuthStateChanged((userCred) => {
+      console.log(userCred);
+      if (userCred) {
         userCred.getIdToken().then((token) => {
-          console.log(token)
-        })
-      }else{
-        setAuth(false)
-        window.localStorage.setItem("auth", "false")
-        navigate("/login")
+          console.log(token);
+        });
+      } else {
+        setAuth(false);
+        window.localStorage.setItem("auth", "false");
+        navigate("/login");
       }
-    })
-   
-  }, [])
-  
+    });
+  }, []);
+
   return (
-    <div className="w-screen h-screen bg-blue-400 flex justify-center items-center">
-    <Routes>
-      <Route path='/login' element={<Login setAuth={setAuth} />} />
-      <Route path='/*' element={<Home />}/>
-    </Routes>
-    </div>
-  )
+    <AnimatePresence exitBeforeEnter>
+      <div className="h-auto min-w-(680px) bg-blue-400 flex justify-center items-center">
+        <Routes>
+          <Route path="/login" element={<Login setAuth={setAuth} />} />
+          <Route path="/*" element={<Home />} />
+        </Routes>
+      </div>
+    </AnimatePresence>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../assets/img/images.png";
+
+const Header = () => {
+  return (
+    <header className="flex items-center w-full p-4 md:py-2 md:px-6">
+      <NavLink to={"/"}>
+        <img src={Logo} alt="music" className="w-16"  />
+      </NavLink>
+      <ul className="flex items-center justify-center ml-7">
+        <li className="mx-5 text-lg"> <NavLink to={"/home"}>Home</NavLink></li>
+        <li className="mx-5 text-lg"> <NavLink to={"/musics"}>Musics</NavLink></li>
+        <li className="mx-5 text-lg"> <NavLink to={"/premium"}>Premium</NavLink></li>
+        <li className="mx-5 text-lg"> <NavLink to={"/contact"}>Contact Us</NavLink></li>
+
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
