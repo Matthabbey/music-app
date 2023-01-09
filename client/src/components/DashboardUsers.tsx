cc019d5c142fd2696e6e67f4512a9a9f507452cc
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-01-09 03:01:38 +0100

    modified route

--- client/src/components/DashboardUsers.tsx
+++ client/src/components/DashboardUsers.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { useStateValue } from "../context/StateProvider";
 
 const DashboardUsers = () => {
+  const [{ user }, dispatch]: any | {} = useStateValue();
   return (
-    <div>DashboardUsers</div>
-  )
-}
+    <div className="w-full p-4 flex items-center justify-center flex-col">
+      <div className="relative w-full py-12 min-h-[400px] overflow-x-scroll scrollbar-thin scrollbar-track-slate-300 scrollbar-thumb-slate-400 my-4 flex flex-col items-center justify-start p-4 border border-gray-300 rounded-md gap-3"></div>
+    </div>
+  );
+};
 
-export default DashboardUsers
+export default DashboardUsers;
