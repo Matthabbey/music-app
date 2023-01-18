b6823f0b66647b200965b27373935a422934ba8f
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-01-18 23:55:48 +0100

    update user role

--- client/src/components/DashboardUsers.tsx
+++ client/src/components/DashboardUsers.tsx
@@ -1,51 +1,75 @@
 import React, { useState } from "react";
 import { getAllUSers } from "../api";
 import { useStateValue } from "../context/StateProvider";
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 import moment from "moment";
 
-export const DashboardUserCard = ({data, index}: any)=>{
+export const DashboardUserCard = ({ data, index }: any) => {
   const [{ user }, dispatch]: any | {} = useStateValue();
-  const [updateRole, setUpdateRole] = useState(false)
+  const [updateRole, setUpdateRole] = useState(false);
   const createdAt = moment(new Date(data.createdAt)).format("MMMM Do YYYY");
-  
+
   return (
-    
-      <motion.div key={index} className="relative w-full rounded-md flex items-center justify-between-py-4 bg-lightOverlay cursor-pointer hover:bg-card hover:shadow-md">
-        <div className="w-275 flex item-center justify-center min-w-[160px]">
-          <img src={data.imageUrl} referrerPolicy="no-referrer" alt="" className="w-10 h-10 object-cover rounded-md min-w-[40px] shadow-md"/>
-        </div>
+    <motion.div
+      key={index}
+      className="relative w-full rounded-md flex items-center justify-between-py-4 bg-lightOverlay cursor-pointer hover:bg-card hover:shadow-md"
+    >
+      <div className="w-275 flex item-center justify-center min-w-[160px]">
+        <img
+          src={data.imageUrl}
+          referrerPolicy="no-referrer"
+          alt=""
+          className="w-10 h-10 object-cover rounded-md min-w-[40px] shadow-md"
+        />
+      </div>
 
-        <p className="text-base text-textColor text-center w-275 min-w-[160px]">{data.name}</p>
-        <p className="text-base text-textColor text-center w-275 min-w-[160px]">{data.email}</p>
-        <p className="text-base text-textColor text-center w-275 min-w-[160px]">{data.email_verified ? "True" : "False"}</p>
-        <p className="text-base text-textColor text-center w-275 min-w-[160px]">{createdAt}</p>
+      <p className="text-base text-textColor text-center w-275 min-w-[160px]">
+        {data.name}
+      </p>
+      <p className="text-base text-textColor text-center w-275 min-w-[160px]">
+        {data.email}
+      </p>
+      <p className="text-base text-textColor text-center w-275 min-w-[160px]">
+        {data.email_verified ? "True" : "False"}
+      </p>
+      <p className="text-base text-textColor text-center w-275 min-w-[160px]">
+        {createdAt}
+      </p>
 
-        <div className="w-275 min-w-[160px] text-center flex items-center justify-center gap-6 relative">
-        <p className="text-base text-textColor text-center w-275 min-w-[160px]">{data.role}</p>
+      <div className="w-275 min-w-[160px] text-center flex items-center justify-center gap-6 relative">
+        <p className="text-base text-textColor text-center w-275 min-w-[160px]">
+          {data.role}
+        </p>
 
-        {
-          data._id !== user?.user._id && (
-            <motion.p whileTap={{scale: 0.75}} className="text-[10px] font-semibold text-textColor px-1 bg-purple-200 rounded-sm hover:shadow-md">{data.role === "admin" ? "Member" : "Admin"}</motion.p>
- 
-          )
-        }
-        {
-          updateRole && (
-            <motion.div className="absolute z-10 top-6 right-6 p-4 flex items-start flex-col gap-4 bg-white shadow-xl rounded-md">
-            <p className="text-textColor text-sm font-semibold">Are you sure, you want to mark user as <span> {data.role === "admin" ? "Member" : "Admin "}</span> ?</p>
-  
+        {data._id !== user?.user._id && (
+          <motion.p
+            whileTap={{ scale: 0.75 }}
+            className="text-[10px] font-semibold text-textColor px-1 bg-purple-200 rounded-sm hover:shadow-md"
+          onClick={()=> setUpdateRole(true)}>
+            {data.role === "admin" ? "Member" : "Admin"}
+          </motion.p>
+        )}
+        {updateRole && (
+          <motion.div className="absolute z-10 top-6 right-6 p-4 flex items-start flex-col gap-4 bg-white shadow-xl rounded-md">
+            <p className="text-textColor text-sm font-semibold">
+              Are you sure, you want to mark user as{" "}
+              <span> {data.role === "admin" ? "Member" : "Admin "}</span> ?
+            </p>
+            <div className="flex items-center gap-4">
+            <motion.button whileTap={{scale: 0.75}} className="outline-none border-none text-sm px-4 py1 rounded-md text-white bg-blue-500 hover:shadow-md ">
+                    Yes
+            </motion.button>
+
+            <motion.button whileTap={{scale: 0.75}} className="outline-none border-none text-sm px-4 py1 rounded-md text-white bg-red-500 hover:shadow-md ">
+                    No
+            </motion.button>
+            </div>
           </motion.div>
-          )
-        }
-       
-        </div>
-
-      </motion.div>
-
-      
-  )
-}
+        )}
+      </div>
+    </motion.div>
+  );
+};
 const DashboardUsers = () => {
   const [{ allUsers }, dispatch]: any | {} = useStateValue();
   return (
@@ -80,18 +104,14 @@ const DashboardUsers = () => {
             Role
           </p>
         </div>
-    {/* table body content */}
-        {
-          allUsers && (
-            allUsers.map((data: string, i: string)=>(
-              <DashboardUserCard key={i} data={data} index={i} />
-            ))
-          )
-        }
+        {/* table body content */}
+        {allUsers &&
+          allUsers.map((data: string, i: string) => (
+            <DashboardUserCard key={i} data={data} index={i} />
+          ))}
       </div>
     </div>
   );
 };
-
 
 export default DashboardUsers;
