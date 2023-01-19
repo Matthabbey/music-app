d1922359aed5c1c3240b5f966cf9796d4014fd2a
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-01-19 02:57:07 +0100

    update of role

--- server/src/controller/user.ts
+++ server/src/controller/user.ts
@@ -1,66 +1,84 @@
-import express, {Request, Response} from "express"
-import admin from "../config/firebase.config"
-import { userModel } from "../models/userModel"
-import { newUserData, updateNewUserData } from "../utils/utils"
+import express, { Request, Response } from "express";
+import admin from "../config/firebase.config";
+import { userModel } from "../models/userModel";
+import { newUserData, updateNewUserData } from "../utils/utils";
 
-
-export const LoginUser = async ( req: Request, res: Response) =>{
-    try {
-        if(!req.headers.authorization){
-            return res.status(404).json({message: "resquest Not  Here Found"})
-        }
-        const token = req.headers.authorization.split(" ")[1]
-        const decodeValue = await admin.auth().verifyIdToken(token)
-        if(!decodeValue){
-            return res.status(505).json({message: "UnAuthorized"})
-        }else{
-
-            const userExist = await userModel.findOne({"user_id": decodeValue.user_id})
-            if(!userExist){
-               newUserData(decodeValue, req, res)
-            }else{
-                updateNewUserData(decodeValue, req, res)
-                // return res.send("need to update")
-            }
-        }
-
-    } catch (error) {
-       return res.status(500).json({
-            Error: error,
-            route: "/user/signup",
-    })
+export const LoginUser = async (req: Request, res: Response) => {
+  try {
+    if (!req.headers.authorization) {
+      return res.status(404).json({ message: "resquest Not  Here Found" });
     }
-}
+    const token = req.headers.authorization.split(" ")[1];
+    const decodeValue = await admin.auth().verifyIdToken(token);
+    if (!decodeValue) {
+      return res.status(505).json({ message: "UnAuthorized" });
+    } else {
+      const userExist = await userModel.findOne({
+        user_id: decodeValue.user_id,
+      });
+      if (!userExist) {
+        newUserData(decodeValue, req, res);
+      } else {
+        updateNewUserData(decodeValue, req, res);
+        // return res.send("need to update")
+      }
+    }
+  } catch (error) {
+    return res.status(500).json({
+      Error: error,
+      route: "/user/signup",
+    });
+  }
+};
 
 export const UpdateUser = async (req: Request, res: Response) => {
-    const filter = { _id: req.params.userId };
-    const songId = req.query;
-  
-    try {
-      console.log(filter, songId);
-      const result = await userModel.updateOne(filter, {
-        $push: { favourites: songId },
-      });
-      res.status(200).send({ success: true, msg: "Song added to favourites" });
-    } catch (error) {
-      res.status(400).send({ success: false, msg: error });
-    }
+  const filter = { _id: req.params.userId };
+  const songId = req.query;
+
+  try {
+    console.log(filter, songId);
+    const result = await userModel.updateOne(filter, {
+      $push: { favourites: songId },
+    });
+    res.status(200).send({ success: true, msg: "Song added to favourites" });
+  } catch (error) {
+    res.status(400).send({ success: false, msg: error });
+  }
+};
+
+export const getAllUSers = async (req: Request, res: Response) => {
+  const options = {
+    // sort returned documents in ascending order
+    sort: { createdAt: 1 },
+    // Include only the following
+    // projection : {}
   };
 
-  export const getAllUSers =  async (req: Request, res: Response) => {
-    const options = {
-      // sort returned documents in ascending order
-      sort: { createdAt: 1 },
-      // Include only the following
-      // projection : {}
-    };
-  
-    const cursor = await userModel.find();
-    if (cursor) {
-      res.status(200).send({ success: true, data: cursor });
-    } else {
-      res.status(200).send({ success: true, msg: "No Data Found" });
-    }
+  const cursor = await userModel.find();
+  if (cursor) {
+    res.status(200).send({ success: true, data: cursor });
+  } else {
+    res.status(200).send({ success: true, msg: "No Data Found" });
+  }
+};
+
+export const updateRole = async (req: Request, res: Response) => {
+  const filter = { _id: req.params.userId };
+  const role = req.body.data.role;
+
+  const options = {
+    upsert: true,
+    new: true,
   };
-
-
+  try {
+    const result = await userModel.findByIdAndUpdate(
+      filter,
+      { role: role },
+      options
+    );
+    return res.status(200).send({ user: result });
+  } catch (error) {
+    res.status(200).send({ success: true, msg: error });
+    console.log(error);
+  }
+};
