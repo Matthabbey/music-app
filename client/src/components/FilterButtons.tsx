b015b1e64aaa24116377ee1296cb04635d2797dd
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-01-21 22:33:23 +0100

    filterNew

--- client/src/components/FilterButtons.tsx
+++ client/src/components/FilterButtons.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoChevronDown } from 'react-icons/io5'
 
 const FilterButtons = ({filterData, flag}: any) => {
+    const [filter, setFilter] = useState(null)
+    const [filterMenu, setFilterMenu] = useState(false)
+
+
   return (
     <div className='border border-gray-300 px-4 py-1 top-3 rounded-md relative cursor-pointer hover:border-gray-400'>
-           <p className='text-base tracking-wide text-textColor flex items-center gap-2'> {flag}
-           <i className='text-base'><IoChevronDown /></i>
+           <p className='text-base tracking-wide text-textColor flex items-center gap-2'> {!filterMenu  && flag}
+           <i className='text-base text-textColor duration-150 transition-all ease-in-out'><IoChevronDown /></i>
             </p>
     </div>
   )
