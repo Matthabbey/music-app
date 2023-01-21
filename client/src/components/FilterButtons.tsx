bef75170714376d15a63c41f5d705e36d77c3ede
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-01-21 23:02:14 +0100

    dropDown

--- client/src/components/FilterButtons.tsx
+++ client/src/components/FilterButtons.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import React, { useState } from 'react'
 import { IoChevronDown } from 'react-icons/io5'
 
@@ -8,9 +9,15 @@ const FilterButtons = ({filterData, flag}: any) => {
 
   return (
     <div className='border border-gray-300 px-4 py-1 top-3 rounded-md relative cursor-pointer hover:border-gray-400'>
-           <p className='text-base tracking-wide text-textColor flex items-center gap-2'> {!filterMenu  && flag}
-           <i className='text-base text-textColor duration-150 transition-all ease-in-out'><IoChevronDown /></i>
+           <p className='text-base tracking-wide text-textColor flex items-center gap-2' onClick={()=>setFilterMenu(!filterMenu )}> {!filterMenu  && flag}
+           <IoChevronDown className='text-base text-textColor duration-150 transition-all ease-in-out' />
             </p>
+            {filterData && filterMenu && (
+
+            <motion.div className='w-48 z-50 backdrop-blur-sm max-h-44 overflow-y-scroll scrollbar-thin scrollbar-track-gray-200 scrollbar-thumb-gray-400 py-2 flex flex-col rounded-md shadow-md absolute top-8 left-1'>hiii
+
+            </motion.div>
+            )}
     </div>
   )
 }
