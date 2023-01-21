9ff3c57c512b3a20e2e1a362f4809ba228cfa030
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-01-21 01:26:04 +0100

    songCard

--- client/src/components/SongCard.tsx
+++ client/src/components/SongCard.tsx
@@ -7,6 +7,9 @@ const SongCard = ({ data, index }: any) => {
         <div className="w-40 min-w-[160px] h-40 min-h-[160px] rounded-lg drop-shadow-lg relative overflow-hidden">
             <motion.img whileHover={{scale: 1.09}} src={data.imageURL} className="w-full h-full rounded-lg object-cover "/>
         </div>
+        <p className="text-base text-headingColor font-semibold py-1">{data.name.length > 25 ? `${data.name.slice(0, 10)}...`: data.name}
+            <span className="block text-sm my-2 text-gray-400">{data.artist}</span>
+        </p>
     </motion.div>
   );
 };
