60a9ccbfc7e70b63bc8e0d93c245d8e95d3e3984
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-01-31 17:26:28 +0100

    artistcommit

--- client/src/components/DashboardArtists.tsx
+++ client/src/components/DashboardArtists.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useStateValue } from "../context/StateProvider";
+import { useEffect } from "react";
+import { getAllArtists } from "../api";
+import { actionType } from "../context/reducer";
+import SongCard from "./SongCard";
 
 const DashboardArtists = () => {
-  return (
-    <div>DashboardArtists</div>
-  )
-}
+  const [{ allArtists }, dispatch]: any = useStateValue();
 
-export default DashboardArtists
+  useEffect(() => {
+    if (!allArtists) {
+      getAllArtists().then((data) => {
+        console.log(data);
+        
+        dispatch({
+          type: actionType.SET_ALL_ARTISTS,
+          allArtists: data.name,
+        });
+      });
+    }
+  }, []);
+  return (
+
+      <div className="w-full p-4 flex items-center justify-center flex-col">
+        <div className="relative border w-full p-4 my-4 py-12 border-gray-300"></div>
+        <ArtistContainer data={allArtists}/>
+      </div>
+  );
+};
+
+export const ArtistContainer = ({ data }: any) => {
+  return (
+    <div className="w-full flex flex-wrap gap-3 items-center justify-evenly">
+      {data &&
+        data.map((song: any, i: string) => (
+          <SongCard key={song._id} data={song} index={i} />
+        ))}
+    </div>
+  );
+};
+
+export default DashboardArtists;
