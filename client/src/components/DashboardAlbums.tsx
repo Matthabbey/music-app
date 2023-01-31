69384a50ece238f69565437d668427ca6f9d6e0d
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-01-31 19:39:09 +0100

    AlbumDashboard

--- client/src/components/DashboardAlbums.tsx
+++ client/src/components/DashboardAlbums.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getAllAlbums } from '../api';
+import { actionType } from '../context/reducer';
+import { useStateValue } from '../context/StateProvider';
+import SongCard from './SongCard';
 
 const DashboardAlbums = () => {
+  const [{ allAlbums }, dispatch]: any = useStateValue();
+
+  useEffect(() => {
+    if (!allAlbums) {
+      getAllAlbums().then((data) => {
+        console.log(data);
+        
+        dispatch({
+          type: actionType.SET_ALL_ARTISTS,
+          allArtists: data.name,
+        });
+      });
+    }
+  }, []);
   return (
-    <div>DashboardAlbums</div>
-  )
+
+      <div className="w-full p-4 flex items-center justify-center flex-col">
+        <div className="relative border w-full p-4 my-4 py-12 border-gray-300"></div>
+        <ArtistContainer data={allAlbums}/>
+      </div>
+  );
+};
+
+export const ArtistContainer = ({ data }: any) => {
+  return (
+    <div className="w-full flex flex-wrap gap-3 items-center justify-evenly">
+      {data &&
+        data.map((song: any, i: string) => (
+          <SongCard key={song._id} data={song} index={i} />
+        ))}
+    </div>
+  );
 }
 
 export default DashboardAlbums
