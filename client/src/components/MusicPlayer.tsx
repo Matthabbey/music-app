0214c9865a224dc6eb38f2d05ac02aa8ef91f4a6
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-07 22:00:23 +0100

    music

--- client/src/components/MusicPlayer.tsx
+++ client/src/components/MusicPlayer.tsx
@@ -7,12 +7,13 @@ import 'react-h5-audio-player/lib/styles.css';
 import { getAllSongs } from '../api';
 import { actionType } from '../context/reducer';
 import { duration } from 'moment';
+import { IoMusicalNote } from 'react-icons/io5';
 
 const MusicPlayer = () => {
   const [{ allSongs, isSongPlaying, songIndex, allAlertMassages}, dispatch]: String | any= useStateValue();
 
   return (
-    <div className='w-full items-center flex gap-3 overflow-hidden'>
+    <div className='w-full items-center flex gap-3'>
         <div className='w-full items-center flex gap-1 p-4 relative'>
             <img src={allSongs[songIndex]?.imageURL} alt="" className='w-20 h-20 object-cover rounded-md'/>
             <div className='flex items-center flex-col'>
@@ -65,15 +66,18 @@ export const PlayListCard = () =>{
         }
     }, [])
 
-    const setCurrentSongIndex = ()=>{
+    const setCurrentSongIndex = (si: any)=>{
         if(!isSongPlaying){
             dispatch({
                 type: actionType.SET_ISSONG_PLAYING,
                 isSongPlaying: true
             })
         }
-        if(song !== songIndex){
-
+        if(songIndex !== si){
+            dispatch({
+                type: actionType.SET_SONG,
+                song: si
+            })
         }
     }
 
@@ -81,9 +85,23 @@ export const PlayListCard = () =>{
         <div className='absolute left-4 bottom-24 gap-2 py-2 w-350 max-w-[350px] h-510 max-h-[510px] flex flex-col overflow-y-scroll scrollbar-thin rounded-md bg-primary shadow-md'>
             {
                 allSongs.length > 0 ? (
-                    allSongs.map((song: string, index: number)=>(
-                        <motion.div initial={{opacity: 0, translateX: -50}} animate={{opacity: 1, translateX: 0}} transition={{duration: 0.3, delay: index * 0.1}} className='group w-full p-4 hover:bg-card flex gap-3 items-center cursor-pointer bg-transparent'>
-
+                    allSongs.map((music: string, index: number)=>(
+                        <motion.div initial={{opacity: 0, translateX: -50}} animate={{opacity: 1, translateX: 0}} transition={{duration: 0.3, delay: index * 0.1}} className='group w-full p-4 hover:bg-card flex gap-3 items-center cursor-pointer bg-transparent'
+                        onClick={()=>setCurrentSongIndex(index)}
+                        key={index}
+                        >
+                           <i className='text-textColor group-hover:text-headingColor text-2xl cursor-pointer'><IoMusicalNote /></i>
+                           <div className='flex items-start flex-col'>
+                            <p className='text-lg text-headingColor font-semibold'>
+                                {`${
+                                    music?.name.length > 20 ? music?.name.slice(0, 20): music?.name 
+                                }`}
+                                <span className='text-base'>({music?.name})</span>
+                            </p>
+                            <p className='text-textColor'>{music?.artist}
+                            <span className='text-sm text-textColor font-semibold'>({music.category})</span>
+                            </p>
+                            </div> 
                         </motion.div>
                     ) )
                 ):<></>
